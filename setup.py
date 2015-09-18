ab8648159353261deba0fcaa5926c9df70d55321
shughes <shughes.uk@gmail.com>
2015-09-17 21:55:38 -0500

    start of py2exe stuff

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 from distutils.core import setup
 import py2exe
 
-setup(console=['subscriber_alerter.py'])
+setup(
+    console=['subscriber_alerter.py'],
+    zipfile=None,
+    options={
+            'py2exe':{
+                    'bundle_files' :1,
+                    'compressed' : True
+                    }
+            }
+    )
