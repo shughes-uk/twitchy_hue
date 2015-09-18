1e8a1413a9ded00e26bc56cf4b6221969613a21f
shughes <shughes.uk@gmail.com>
2015-09-17 22:03:05 -0500

    fix py2exe

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 import py2exe
 
 setup(
-    console=['subscriber_alerter.py'],
+    console=['twitchy_hue.py'],
     zipfile=None,
     options={
             'py2exe':{
-                    'bundle_files' :1,
+                    'bundle_files' : 1,
                     'compressed' : True
                     }
             }
